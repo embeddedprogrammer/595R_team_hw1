80e3133d475c88fc46a5c262065997328f7e7e33
Unknown <snowflakeobsidian3.14@gmail.com>
2017-10-18 12:48:21 -0600

    Remove some unnecessary files.

--- controller.m
+++
@@ -1,8 +0,0 @@
-function u = controller(uu, P)
-	% process inputs to function
-	xhat    = uu(1:3);
- 	t       = uu(4);
-	v     = 1 + 0.5*cos(2*pi*0.2*t);
-	omega = -0.2 + 2*cos(2*pi*0.6*t);
-	u = [v; omega];
-end
